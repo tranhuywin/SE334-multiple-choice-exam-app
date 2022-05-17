4ae2a93c7bea1be65c1f20f7d21fecc9f903db3b
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2022-05-17 22:33:35 +0700

    Create Subject Page

--- client/test_app/src/components/exam/subjects-list.jsx
+++ client/test_app/src/components/exam/subjects-list.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 import "./subjects-list.css"
 import Exam from "./exam.jsx"
 
@@ -31,12 +33,29 @@ function SubjectsList() {
 
     return (
         <div>
-            <ul>
+            <ul className="subject-list">
+                <h4>Đề Thi Trắc Nghiệm THPT Quốc Gia môn {getPathName} 2022</h4>
                 {subjects.map(sub => {
                     return (
-                        (
-                            <li key={sub.id}>{sub.title || sub.name}</li>
-                        )
+                            <li key={sub.id} className="subject-item">
+                                <div className="subject-item__title">Đề Thi Trắc Nghiệm THPT QG năm 2022 môn {getPathName}</div>
+                                <div className="subject-item__school">Trường THPT ABC</div>
+                                <div className="subject-item__calendar">
+                                    <FontAwesomeIcon icon={faCalendarDays} className="subject-item__calendar-icon" />
+                                    <span>20/20/2022</span>
+                                </div>
+
+                                <div className="progress-box">
+                                    <div className="progress-box__item">
+                                        <span>0/50</span>
+                                        <div className="progress">
+                                            <div className="progress-bar"></div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <a href="/exam/:name" className="btn-starting-test">Bắt đầu thi</a>
+                            </li>
                     )
                 })}
             </ul>
