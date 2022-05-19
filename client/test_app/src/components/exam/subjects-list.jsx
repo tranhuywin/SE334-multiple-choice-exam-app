4904e82323104a77fe5cd00f960bed59b3ad14af
thanghoang <hoanganhthang357@gmail.com>
2022-05-19 16:53:23 +0700

    Update Exam Page

--- client/test_app/src/components/exam/subjects-list.jsx
+++ client/test_app/src/components/exam/subjects-list.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 import "./subjects-list.css"
-import Exam from "./exam.jsx"
+// import Header from "../pages/header.jsx"
 
 
 // const subjectList = ["posts", "comments", "albums", "photos", "todos", "users"]
