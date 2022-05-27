8cb19238f606aec72bcda665ab22dd99b56ab089
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2022-05-27 20:23:22 +0700

    Update Tests Page

--- client/test_app/src/components/exam/tests.jsx
+++ client/test_app/src/components/exam/tests.jsx
@@ -3,14 +3,65 @@ import { Route, Switch } from "react-router-dom";
 import  { BrowserRouter as Router } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
+import { useRef } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-solid-svg-icons';
 import "./tests.css";
 
 
-function test () {
+function Test () {
+  const Ref = useRef(null);
+  const [timer, setTimer] = useState('00:00:00');
+
+  const getTimeRemaining = (e) => {
+      const total = Date.parse(e) - Date.parse(new Date());
+      const seconds = Math.floor((total / 1000) % 60);
+      const minutes = Math.floor((total / 1000 / 60) % 60);
+      const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+      return {
+          total, hours, minutes, seconds
+      };
+  }
+
+  const startTimer = (e) => {
+      let { total, hours, minutes, seconds } 
+                  = getTimeRemaining(e);
+      if (total >= 0) {
+          setTimer(
+              (hours > 9 ? hours : '0' + hours) + ':' +
+              (minutes > 9 ? minutes : '0' + minutes) + ':'
+              + (seconds > 9 ? seconds : '0' + seconds)
+          )
+      }
+  }
+
+
+  const clearTimer = (e) => {
+      setTimer('01:30:00');
+      if (Ref.current) clearInterval(Ref.current);
+      const id = setInterval(() => {
+          startTimer(e);
+      }, 1000)
+      Ref.current = id;
+  }
+
+  const getDeadTime = () => {
+      let deadline = new Date();
+      deadline.setSeconds(deadline.getSeconds() + 5400);
+      return deadline;
+  }
+
+  useEffect(() => {
+      clearTimer(getDeadTime());
+  }, []);
+
+  const onClickReset = () => {
+      clearTimer(getDeadTime());
+  }
+
     return (
-           <div className="container-test">
+        <div className="container-test">
+            <div className="doing-tests">
                 <span>Câu 1</span>
                 <div className="container-test__question">Nhằm giúp các em chuẩn bị thật tốt cho kì thi THPT QG môn Toán sắp đến. Xin gửi đến các em bộ đề thi trắc nghiệm THPT QG môn Toán được sưu tập qua các năm với đầy đủ lời giải chi tiết.</div>
                 <div className="container-test__answer">
@@ -29,13 +80,17 @@ function test () {
                     <input type="radio" id="answer-d" value="A. ABC"/>
                     <label for="answer-a">A. ABC</label><br></br>
                 </div>
+            </div>
 
-                <div className="time-and-next-or-prev">
-                    <FontAwesomeIcon icon={faClock} />
-                    
+            <div className="testing-time-container">
+                <div className="testing-time">
+                    <FontAwesomeIcon icon={faClock} className="testing-time-icon"/>
+                    <span className="countdown-timer">{timer}</span>
                 </div>
-           </div>
+                <button className="testing-time-btn">Kết thúc bài thi</button>
+            </div>
+        </div>
     )
 }
 
-export default test;
+export default Test;
