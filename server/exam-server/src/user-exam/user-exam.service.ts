10e2f20568c7346211ee52be4e510c939ff4c935
Win Tran <winth@dgroup.co>
2022-06-19 20:51:12 +0700

    feat: exam

--- server/exam-server/src/user-exam/user-exam.service.ts
+++ server/exam-server/src/user-exam/user-exam.service.ts
@@ -1,21 +1,63 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ExamsService } from 'src/exams/exams.service';
+import { UsersService } from 'src/users/users.service';
 import { Repository } from 'typeorm';
 import { CreateUserExamDto } from './dto/create-user-exam.dto';
 import { UpdateUserExamDto } from './dto/update-user-exam.dto';
+import { CorrectAnswer } from './entities/correct-answer.entity';
 import { UserExam } from './entities/user-exam.entity';
 
 @Injectable()
 export class UserExamService {
   constructor(
     @InjectRepository(UserExam)
-    private readonly userExamRepository: Repository<UserExam>,
+    private readonly userExamRepo: Repository<UserExam>,
+    @InjectRepository(CorrectAnswer)
+    private readonly correctAnswerRepo: Repository<CorrectAnswer>,
     private readonly examsService: ExamsService,
-  ){}
+    private readonly usersService: UsersService,
+  ) { }
 
-  public async create(createUserExamDto: CreateUserExamDto) {
-    
+  public async create(createUserExamDto: CreateUserExamDto, userId: string) {
+    const exam = await this.examsService.findOne(createUserExamDto.examId);
+    const userExam = new UserExam();
+
+    //caculate score
+    let score = 0;
+    const correctAnswers = createUserExamDto.questions.map((question, index) => {
+      let isCorrect = false;
+      if (createUserExamDto.answers[index] === 1 && exam.questions[index].answerA.isCorrect) {
+        score++;
+        isCorrect = true;
+      }
+      if (createUserExamDto.answers[index] === 2 && exam.questions[index].answerB.isCorrect) {
+        score++;
+        isCorrect = true;
+      }
+      if (createUserExamDto.answers[index] === 3 && exam.questions[index].answerC.isCorrect) {
+        score++;
+        isCorrect = true;
+      }
+      if (createUserExamDto.answers[index] === 4 && exam.questions[index].answerD.isCorrect) {
+        score++;
+        isCorrect = true;
+      }
+      // create correctAnswer
+      const correctAnswer = new CorrectAnswer();
+      correctAnswer.questionId = exam.questions[index].id;
+      correctAnswer.isCorrect = isCorrect;
+      correctAnswer.userExam = userExam;
+      return correctAnswer;
+    });
+    //create userExam
+    userExam.score = score;
+    userExam.exam = exam;
+    userExam.user = await this.usersService.findOne(userId);
+    const data = await this.userExamRepo.save(userExam);
+    //create correctAnswer
+    await this.correctAnswerRepo.save(correctAnswers);
+    return data;
   }
 
   findAll() {
